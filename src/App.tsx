b85e45d6753d3b8a840f742f3853e6379a13e9a8
= <daniell.felipe.2017@gmail.com>
2025-02-15 12:04:14 -0300

    setup global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import styles from './App.module.css'
-
 import './global.css'
 
-function App() {
-  return (
-    <h2>Hello World</h2>
-  )
+export function App() {
+  return <h2>Hello World</h2>
 }
-
-export default App
