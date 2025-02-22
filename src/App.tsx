02f297ede8122e6586180dc12d0d2035e23fa470
= <daniell.felipe.2017@gmail.com>
2025-02-22 15:18:58 -0300

    add task input

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,22 @@ import Header from './components/Header';
 import styles from './App.module.css';
 
 import './global.css'
+import { PlusCircle } from '@phosphor-icons/react';
 
 export function App() {
+
   return (
     <main className={styles.wrapper}>
       <Header />
+      <section>
+        <form>
+          <input type="text" placeholder="Adicione uma nova tarefa" />
+          <button>
+            Criar
+            <PlusCircle size={16} />
+          </button>
+        </form>
+      </section>
     </main>
   )
 }
